d111097e0ea78804a1c7ae4a2abf743bd215da10
shpaq23 <shpaq23@gmail.com>
2019-03-17 14:18:44 +0100

    first character

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TavernModule} from './tavern/tavern.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TavernModule
   ],
   providers: [],
   bootstrap: [AppComponent]
